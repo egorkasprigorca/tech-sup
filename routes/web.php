02cf42fa5b2e5fdf3484561d39fcbe1e3d4caf88
Egorkasprigorca <chief.bolotow2013@gmail.com>
2021-10-18 11:11:24 +0700

    Add sign-up, sign-in and logout systems.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SignUpController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogOutController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,4 +14,19 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::redirect('/', '/board');
+
+Route::get('/board', function (){
+    return view('board/board', ['userName' => \Illuminate\Support\Facades\Auth::user()->name]);
+})->middleware('auth');
+
+Route::view('/signup', 'auth/signup');
+Route::post('/signup', [SignUpController::class, 'registration']);
+
+Route::view('/login', 'auth/login')->name('login');
+Route::post('/login', [LoginController::class, 'auth']);
+
+Route::get('/board/logout', [LogOutController::class, 'logout']);
+
 
